a29f76af2d10c9789fbbc40700845125a3e0a6ac
davisc0801 <davisc1014@gmail.com>
2019-04-15 15:55:49 -0600

    Add final decryption tests

--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -3,21 +3,26 @@ require "./lib/decrypt"
 
 class DecryptTest < Minitest::Test
 
-    def setup
-      @encrypt = Decrypt.decode("keder ohulw!", "02715", "040895")
-    end
+  def setup
+    @decrypt = Decrypt.decode("keder ohulw!", "02715", "040895")
+  end
 
-    def test_it_accepts_a_date
-      assert_equal @decrypt[:date], "040895"
-    end
+  def test_it_accepts_a_date
+    assert_equal @decrypt[:date], "040895"
+  end
 
-    def test_it_accepts_a_key
-      assert_equal @decrypt[:key], "02715"
-    end
+  def test_it_accepts_a_key
+    assert_equal @decrypt[:key], "02715"
+  end
 
-    def test_it_decrypts_message
-      @decrypt = Decrypt.decode("hello world!", "02715", "040895")
-      assert_equal @decrypt[:decryption], "hello world!"
-    end
+  def test_it_decrypts_message
+    assert_equal @decrypt[:decryption], "hello world!"
+  end
+
+  def test_setup_values
+    decrypt_setup = Decrypt.setup("12345", "080196")
+    assert_equal decrypt_setup[:base], Enigma.generate_characters()
+    assert_equal decrypt_setup[:keys], [20, 27, 35, 51]
+  end
 
 end
