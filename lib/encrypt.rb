4924cddfbd1f4229fc879882bee49d9003738fd9
davisc0801 <davisc1014@gmail.com>
2019-04-15 17:06:50 -0600

    Add guard clauses

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,3 +1,12 @@
 class Encrypt
 
+  inputs = ARGV
+
+  abort("Error - input file not found") if !File.readable?(inputs.first)
+  abort("Error - wrong number of inputs") if inputs.length != 2
+
+  
+
+  puts "Created #{inputs.last} with the key #{[:key]} and date #{[:date]}"
+
 end
