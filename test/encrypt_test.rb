1a6abb25a7627a8376fd02cdd085ba33e2e24e5d
davisc0801 <davisc1014@gmail.com>
2019-04-14 19:07:14 -0600

    Add test helper and refactor tests to reflect class methods.

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -1,10 +1,10 @@
-require "minitest"
+require "./test_helper"
 require "./lib/encrypt"
 
 class EncryptTest < Minitest::Test
 
   def setup
-    @encrypt = Encrypt.new("hello world")
+    @encrypt = Encrypt.encode("hello world")
   end
 
   def test_it_exists
@@ -12,24 +12,24 @@ class EncryptTest < Minitest::Test
   end
 
   def test_it_accepts_a_date
-    encrypt_date = Encrypt.new("hello world", "12345", "080196")
-    assert_equal encrypt_date.date, "080196"
+    encrypt_date = Encrypt.encode("hello world", "12345", "080196")
+    assert_equal encrypt_date[:date], "080196"
   end
 
   def test_it_accepts_a_key
-    encrypt_key = Encrypt.new("hello world", "12345")
-    assert_equal encrypt_key.key, "12345"
+    encrypt_key = Encrypt.encode("hello world", "12345")
+    assert_equal encrypt_key[:key], "12345"
   end
 
   def test_it_has_default_values
     d = Date.today
-    assert_equal @encrypt.date, d.strftime('%d%m%y')
-    assert (1..99999).to_a.include?(@encrypt.key)
+    assert_equal @encrypt[:date], d.strftime('%d%m%y')
+    assert (1..99999).to_a.include?(@encrypt[:key])
   end
 
   def test_it_encrypts_message
-    encrypt_message = Encrypt.new("hello world", "02715", "040895")
-    assert_equal encrypt_message.encryption, "keder ohulw"
+    encrypt_message = Encrypt.encode("hello world", "02715", "040895")
+    assert_equal encrypt_message[:encryption], "keder ohulw"
   end
 
 end
