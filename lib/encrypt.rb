f5342caef4ab0ea0184cbb1946f9188f0bf1bd5f
davisc0801 <davisc1014@gmail.com>
2019-04-16 09:00:05 -0600

    Add decrypt functionality

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -7,11 +7,11 @@ class Encrypt
   abort("Error - wrong number of inputs") if inputs.length != 2
   abort("Error - input file not found") if !File.readable?(inputs.first)
 
-  output = {}
-  
+  output = {:key => Encryption.fetch_key}
+
   File.open(inputs.last, "w+") do |file|
     File.readlines(inputs.first).each do |line|
-      output = Encryption.encode(line.chomp)
+      output = Encryption.encode(line.chomp, output[:key])
       file.puts(output[:encryption])
     end
   end
