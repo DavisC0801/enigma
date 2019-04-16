9140b873e67eedc845d2a9a6c9ab577e9c673770
davisc0801 <davisc1014@gmail.com>
2019-04-16 15:12:09 -0600

    Add CLI for crack.rb

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,3 +1,26 @@
+require "./lib/decryption"
+require "./lib/encryption"
+require "./lib/cracker"
+
 class Crack
+
+  inputs = ARGV
+
+  abort("Error - wrong number of inputs") if inputs.length < 2 || inputs.length > 3
+  abort("Error - input file not found") if !File.readable?(inputs.first)
+
+  output = {
+    :date => Encryption.fetch_date
+  }
+  output[:date] = inputs[2] if inputs.length == 3
+
+  File.open(inputs[1], "w+") do |file|
+    File.readlines(inputs.first).each do |line|
+      output = Cracker.crack(line.chomp, output[:date])
+      file.puts(output[:decryption])
+    end
+  end
+
+  puts "Created #{inputs[1]} with the key #{output[:key]} and date #{output[:date]}"
 
 end
