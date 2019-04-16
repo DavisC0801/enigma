1223a802ee3125abb6976302bc8ee30b0e71f267
davisc0801 <davisc1014@gmail.com>
2019-04-16 17:44:40 -0600

    Finalize CLI interface validation for decrypt functionality

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -6,12 +6,16 @@ class Decrypt
 
   abort("Error - wrong number of inputs") if inputs.length != 4
   abort("Error - input file not found") if !File.readable?(inputs.first)
+  if (inputs.length == 3 && !(1..99999).to_a.include?(inputs[2].to_i)) \
+  || (inputs.length == 4 && !(1..999999).to_a.include?(inputs[3].to_i))
+    abort("Error - invalid input")
+  end
 
   output = {}
   enigma = Enigma.new
   File.open(inputs[1], "w+") do |file|
     File.readlines(inputs.first).each do |line|
-      output = enigma.decode(line.chomp, inputs[2], inputs[3])
+      output = enigma.decrypt(line.chomp, inputs[2], inputs[3])
       file.puts(output[:decryption])
     end
   end
