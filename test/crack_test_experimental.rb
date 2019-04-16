ff0cec2842b0e23f4ed5c95a2d54b97c35e322e3
davisc0801 <davisc1014@gmail.com>
2019-04-16 15:38:05 -0600

    Add experimental crack CLI tests.

--- test/crack_test_experimental.rb
+++ test/crack_test_experimental.rb
@@ -1,26 +1,25 @@
 require "./test/test_helper"
-require "./lib/decrypt"
+require "./lib/crack"
 
-class DecryptTest < Minitest::Test
-
+class CrackTest < Minitest::Test
   def test_it_aborts_if_file_not_found
     assert_output('', "Error - input file not found") do
-      decrypt "tests" "tests" "tests" "tests"
+      Crack "tests" "tests"
     end
   end
 
   def test_it_aborts_if_incorrect_number_of_inputs
     assert_output('', "Error - wrong number of inputs") do
-      decrypt "tests" "tests" "tests" "tests" "tests"
+      Crack "tests" "tests" "tests" "tests"
     end
     assert_output('', "Error - wrong number of inputs") do
-      decrypt "tests" "tests"
+      Crack "tests"
     end
   end
 
   def test_it_outputs_a_file
-    assert_output("Created \"decrypted.txt\" with the key #{/*/} and date #{/*/}", '') do
-      decrypt "encrypted.txt" "decrypted.txt" 83132 160419
+    assert_output("Created \"cracked.txt\" with the key #{/*/} and date #{/*/}", '') do
+      Crack "encrypted.txt" "cracked.txt"
     end
   end
 end
