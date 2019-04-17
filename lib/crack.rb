f9561b81ca325124645800ca85c691259de1171c
davisc0801 <davisc1014@gmail.com>
2019-04-17 09:56:55 -0600

    Refactor crack to run a single pass.

--- lib/crack.rb
+++ lib/crack.rb
@@ -10,13 +10,18 @@ class Crack
   abort("Error - input file not found") if !File.readable?(inputs.first)
 
   output = {
+<<<<<<< Updated upstream
     :date => Encryption.fetch_date
+=======
+    :date => EnigmaSetup.fetch_date,
+>>>>>>> Stashed changes
   }
   output[:date] = inputs[2] if inputs.length == 3
 
   File.open(inputs[1], "w+") do |file|
+    output = Cracker.crack(File.readlines(inputs.first).last.chomp, output[:date])
     File.readlines(inputs.first).each do |line|
-      output = Cracker.crack(line.chomp, output[:date])
+      output = Decryption.decode(line.chomp, output[:key], output[:date])
       file.puts(output[:decryption])
     end
   end
